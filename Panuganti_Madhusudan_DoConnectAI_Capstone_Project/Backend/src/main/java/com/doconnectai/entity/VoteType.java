078de872b97db139901e44cda5ef4d90f66c646c
madhu <madhusudhanp.4u@gmail.com>
2026-06-18 23:52:19 +0530

    Removed unnecessary files

--- Panuganti_Madhusudan_DoConnectAI_Capstone_Project/Backend/src/main/java/com/doconnectai/entity/VoteType.java
+++
@@ -1,9 +0,0 @@
-package com.doconnectai.entity;
-
-public enum VoteType {
-	
-	UPVOTE,
-	
-	DOWNVOTE
-
-}
--- Panuganti_Madhusudan_DoConnectAI_Capstone_Project/Frontend/src/services/userService.js
+++
@@ -1,9 +0,0 @@
-import api from "./api";
-
-export const getAllUsers =
-    () => api.get("/users/all");
-
-export const deleteUser = id =>
-    api.delete(`/users/${id}`);
-
-
